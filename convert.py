1ee66106454fbfd774b8a39523655d5f5a9d2e78
Chmielewski Bartosz <T0323061@gemalto.com>
2025-12-31 22:54:15 +0100

    Minor Changes

--- convert.py
+++
@@ -1,8 +0,0 @@
-import json;
-import hashlib;
-
-data=json.load(open("UserData.json"));
-for k,v in data.items():
-    v["username"] = v["first_name"] + v["last_name"][0];
-    if "password" not in v: v["password"]=hashlib.md5(v["email"].encode()).hexdigest();
-json.dump(data, open("UserData2.json","w"), indent=2)
